3b2cc32a695c5db85f223e712bc68d13a5d975ab
Felipe da Silva Rodrigues <felipedasr3232@gmail.com>
2024-09-04 22:57:50 -0300

    feature/visual: Iniciando a responsividade

---
+++ assets/js/menu.js
@@ -0,0 +1,9 @@
+function toggleNavigation() {
+    var nav = document.querySelector('.testeNav');
+    if (nav.style.display === 'flex') {
+      nav.style.display = 'none';
+    } else {
+      nav.style.display = 'flex';
+    }
+  }
+  
